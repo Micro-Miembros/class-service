dcd156634165b5c1ed42d2541ed62bf12969d0d8
jpnino1018 <juan.nino@u.icesi.edu.co>
2025-09-04 16:44:05 -0500

    fixed feign clients

--- src/main/java/co/analisys/gimnasio/clients/MemberClient.java
+++ src/main/java/co/analisys/gimnasio/clients/MemberClient.java
@@ -3,7 +3,7 @@ package co.analisys.gimnasio.clients;
 import org.springframework.cloud.openfeign.FeignClient; 
 import org.springframework.web.bind.annotation.GetMapping; 
 import org.springframework.web.bind.annotation.PathVariable; 
-@FeignClient(name = "member-service", url = "http://localhost:8081") 
+@FeignClient(name = "member-service", url = "http://member-service:8081") 
 public interface MemberClient { 
     @GetMapping("/api/gimnasio/miembros/{miembroId}/activa") 
     Boolean isMiembroActivo(@PathVariable("miembroId") Long id); 
--- src/main/java/co/analisys/gimnasio/clients/TrainerClient.java
+++ src/main/java/co/analisys/gimnasio/clients/TrainerClient.java
@@ -3,7 +3,7 @@ package co.analisys.gimnasio.clients;
 import org.springframework.cloud.openfeign.FeignClient; 
 import org.springframework.web.bind.annotation.PostMapping; 
 import org.springframework.web.bind.annotation.PathVariable; 
-@FeignClient(name = "trainer-service", url = "http://localhost:8084") 
+@FeignClient(name = "trainer-service", url = "http://trainer-service:8084") 
 public interface TrainerClient { 
     @PostMapping("/api/gimnasio/entrenadores/{entrenadorId}/reservar") 
     Boolean reservarEntrenador(@PathVariable("entrenadorId") Long id); 
