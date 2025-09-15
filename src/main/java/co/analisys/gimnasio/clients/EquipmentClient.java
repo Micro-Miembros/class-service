0d9146dc169b35722ba484834212d16f4d68a56f
Brayan Ortega <brayansteven0322@gmail.com>
2025-09-15 11:19:38 -0500

    update ports for docker

--- src/main/java/co/analisys/gimnasio/clients/EquipmentClient.java
+++ src/main/java/co/analisys/gimnasio/clients/EquipmentClient.java
@@ -3,7 +3,7 @@ package co.analisys.gimnasio.clients;
 import org.springframework.cloud.openfeign.FeignClient; 
 import org.springframework.web.bind.annotation.PathVariable; 
 import org.springframework.web.bind.annotation.PostMapping; 
-@FeignClient(name = "equipment-service", url = "http://localhost:8082") 
+@FeignClient(name = "equipment-service", url = "${equipment.service.url:http://localhost:8082}") 
 public interface EquipmentClient { 
     @PostMapping("/api/gimnasio/equipos/{equipoId}/reservar/{cantidad}") 
     Boolean reservarEquipo(@PathVariable("equipoId") Long id, @PathVariable("cantidad") Long cantidad);
--- src/main/java/co/analisys/gimnasio/clients/MemberClient.java
+++ src/main/java/co/analisys/gimnasio/clients/MemberClient.java
@@ -3,7 +3,8 @@ package co.analisys.gimnasio.clients;
 import org.springframework.cloud.openfeign.FeignClient; 
 import org.springframework.web.bind.annotation.GetMapping; 
 import org.springframework.web.bind.annotation.PathVariable; 
-@FeignClient(name = "member-service", url = "http://localhost:8081") 
+
+@FeignClient(name = "member-service", url = "${member.service.url:http://localhost:8081}") 
 public interface MemberClient { 
     @GetMapping("/api/gimnasio/miembros/{miembroId}/activa") 
     Boolean isMiembroActivo(@PathVariable("miembroId") Long id); 
